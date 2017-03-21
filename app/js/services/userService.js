99b666170a80c1cabe4368cef9cc9884781735ed
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2017-03-21 11:30:36 +0100

    Update userService.js

--- app/js/services/userService.js
+++ app/js/services/userService.js
@@ -11,7 +11,7 @@ angular.module('app')
                 return $http.put('/users/' + id, user);
             },
             delete: function(id) {
-                return $http.put('/users/' + id);
+                return $http.delete('/users/' + id);
             }
         };
     });
